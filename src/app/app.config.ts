6f2000e16a78b976b891ac536a520462f55f40ec
gerwenrick <rickvvgerwen@live.nl>
2024-09-22 17:24:07 +0200

    fix: update reducers to be more modular

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,19 +6,16 @@ import {
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideState, provideStore } from '@ngrx/store';
+import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
-import {
-  PRODUCT_FEATURE_KEY,
-  productsReducer,
-} from './state/products/products.reducer';
+import { appReducer } from './state/app.reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideStore({ [PRODUCT_FEATURE_KEY]: productsReducer }),
+    provideStore(appReducer),
     provideEffects(),
     provideStoreDevtools({
       maxAge: 25,
